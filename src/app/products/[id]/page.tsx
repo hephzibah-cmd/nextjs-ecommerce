6d74ad5f2e0552d53a852d908cebeb4a93a19f36
hephzibah-cmd <82283280+hephzibah-cmd@users.noreply.github.com>
2025-05-29 16:49:43 +0100

    navbar and footer

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,63 @@
+import { prisma } from "@/lib/db/prisma";
+import { notFound } from "next/navigation";
+import Image from "next/image";
+import PriceTag from "@/components/PriceTag";
+import { Metadata } from "next";
+import { cache } from "react";
+import { title } from "process";
+import AddToCartButton from "./AddToCartButton";
+import { incrementProductQuantity } from "./actions";
+
+interface ProductPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Naijmazon",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  //   const product = await prisma.product.findUnique({ where: { id } });
+  //   if (!product) notFound();
+
+  const product = await getProduct(id);
+
+  return (
+    <div className="lg:items-centre flex flex-col gap-4 lg:flex-row">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={500}
+        height={500}
+        className="rounded-lg"
+        priority
+      />
+      <div>
+        <h1 className="text-5x1 font-bold">{product.name}</h1>
+        <PriceTag price={product.price} className="mt-4" />
+        <p className="py-6">{product.description}</p>
+        <AddToCartButton productId={product.id} incrementProductQuantity={incrementProductQuantity} />
+      </div>
+    </div>
+  );
+}
